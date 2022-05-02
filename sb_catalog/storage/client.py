dd07e815d3635312ed0a6995bffa26fbbb00ac6a
Jerrys9 <Jerry048@revature.net>
2022-05-03 02:09:59 +0300

    Implement `sb_catalog.storage.client`

---
+++ sb_catalog/storage/client.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+_db_client = None
+
+
+def get_db_client():
+    global _db_client
+
+    if _db_client is None:
+        _db_client = MongoClient()
+
+    return _db_client
+
+
+def get_db_collection():
+    return get_db_client().sb_catalog.teams
