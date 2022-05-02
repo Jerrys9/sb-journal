e6981eb3376d3d637ffc8c94c4b0d50ce2e48ac0
Jerrys9 <Jerry048@revature.net>
2022-05-03 02:28:39 +0300

    Implement `sb_catalog.storage.crud.get`

---
+++ sb_catalog/storage/crud.py
@@ -0,0 +1,12 @@
+from sb_catalog.storage.client import get_db_collection
+
+
+def get(name=None):
+    db_collection = get_db_collection()
+
+    if name is None:
+        query = {}
+    else:
+        query = {"name": name}
+
+    return db_collection.find(query)
